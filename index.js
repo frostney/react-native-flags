91f7ed2f347259da93da5edc3d6380311675835b
Alexis Puga <alexispuga@pm.me>
2021-11-09 17:17:30 -0600

    feat: load flags asynchronously
    Import flags asynchronously inside a useEffect hook, and set the Image
    source if component is mounted, with the help of a useState hook.
    Remove ./flags import from the top.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // @flow
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image } from 'react-native';
-import * as flags from './flags';
 
 type Props = {
   size: 16 | 24 | 32 | 48 | 64,
@@ -12,12 +11,33 @@ type Props = {
 };
 
 const Flag = ({ size = 64, code, type = 'shiny', style }: Props) => {
-  const flag = flags[type][`icons${size}`][code];
-  const unknownFlag = flags[type][`icons${size}`]['unknown'];
+  // @TODO Set an initial value.
+  const [source, setSource] = useState();
+
+  // Load flags asynchronously.
+  useEffect(() => {
+    let isMounted = true;
+
+    (async () => {
+      const flags = await import('./flags');
+      const flag = flags[type][`icons${size}`][code];
+      const unknownFlag = flags[type][`icons${size}`]['unknown'];
+
+      if (isMounted) {
+        setSource(flag || unknownFlag);
+      }
+    })();
+
+    return () => {
+      // Anti-pattern.
+      // @TODO Abort import instead.
+      isMounted = false;
+    };
+  }, [type, size, code, setSource]);
 
   return (
     <Image
-      source={flag || unknownFlag}
+      source={source}
       style={[{ width: size, height: size }, style]}
     />
   );
